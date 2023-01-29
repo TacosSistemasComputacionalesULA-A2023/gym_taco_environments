c0c1098a31b424ced0e36c705bf7400a79fb7814
Jorge Luis Rodríguez González <egroj97@gmail.com>
2023-01-28 21:25:09 -0400

    Frozen Lake V0

--- gym_taco_environments/__init__.py
+++ gym_taco_environments/__init__.py
@@ -4,3 +4,8 @@ register(
     id='TwoArmedBandit-v0',
     entry_point='gym_taco_environments.envs:TwoArmedBanditEnvV0',
 )
+
+register(
+    id='FrozenLake-v0',
+    entry_point='gym_taco_environments.envs:FrozenLakeEnvV0',
+)
--- gym_taco_environments/envs/__init__.py
+++ gym_taco_environments/envs/__init__.py
@@ -1,1 +1,2 @@
 from gym_taco_environments.envs.bandits.v0.bandits import TwoArmedBanditEnv as TwoArmedBanditEnvV0
+from gym_taco_environments.envs.frozen_lake.v0.frozen_lake import FrozenLakeEnv as FrozenLakeEnvV0
